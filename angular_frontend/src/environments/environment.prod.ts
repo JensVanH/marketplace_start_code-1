05a84a7429e673f089c5c15919e991788e1ed4f1
Derave Thomas <thomas.derave@ugent.be>
2023-10-23 12:09:14 +0200

    for build

--- angular_frontend/src/environments/environment.prod.ts
+++ angular_frontend/src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  baseUrlApi: "https://ugmarket.ugent.be/api2/"
+  baseUrlApi: "https://ugmarket.ugent.be/api2/",
   // baseUrlApi: 'http://localhost:3001'
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoidGRyYXZlIiwiYSI6ImNsbXFjOXVyajAyZWcybW1rOHN2b3JtdTgifQ.5F-rEe5HE5D0JK9f55mxSw' 
+  },
 };
